405b60f215f271b07a2d6c45586d46e31e415218
IgorSoaresss <165588682+IgorSoaresss@users.noreply.github.com>
2024-05-13 19:15:32 -0300

    Update crud_CLASSE.py

--- crud_CLASSE.py
+++ crud_CLASSE.py
@@ -1,13 +1,16 @@
 import os
 
+condominio = dict(list)
+condsCadastrados = list()
+
 def menu():
     print('-='*7)
     print('IDENTIFICAÇÃO')
     print('-='*7)
 
     print('1 - Fazer login')
-    print('2 - Novo na ferramenta (obter ID)')
-    print('3 - Sou DEV')
+    print('2 - Obter ID (novo na ferramenta)')
+    print('0 - Sair')
 
 def choice():
     opcao = int(input('Insira o número aqui -> '))
@@ -18,35 +21,35 @@ def choice():
             print('LOGIN - MORADOR/GESTOR')
             print('-='*4)
 
-            id = str(input('Insira seu ID -> ')
+            id = str(input('Insira o ID do condomínio -> ')
+            classe = str(input('Insira o seu CPF -> ')
             #PUXAR FUNÇÃO DA PAGINA INICIAL MORADOR/GESTOR
         
         case 2:
             print('-='*15)
             print('CADASTRAR NA PLATAFORMA E OBTER ID')
+            print('Apenas gestores e síndicos')
             print('-='*15)
+                     
+            print('Insira suas informações requisitadas abaixo:')
+            condominio['nomeGestor'] = str(input('Nome: '))
+            condominio['cargoGestor'] = str(input('Cargo/função: '))
+            condominio['emailGestor'] = str(input('E-mail: '))
+            condominio['cpfGestor] = str(input('CPF: '))
+                     
+            print('Insira as informações requisitadas abaixo, acerca do condomínio:')
+            condominio['estado'] = str(input('Estado: '))
+            condominio['municipio'] = str(input('Município: '))
+            condominio['bairro'] = str(input('Bairro: '))
+            condominio['ruav'] = str(input('Rua/avenida: '))
+            condominio['numero'] = str(input('Número: '))
+            condominio['nomeCondo'] = str(input('Nome do condomínio: '))
 
-            print('Selecione o que você deseja cadatrar)
-            print('1 - Quero cadastrar meu condomínio (gestor)')
-            print('2 - Quero me cadastrar (morador)')
-            print('3 - Voltar ao menu')
+            condsCadastrados.append(condominio.copy())
 
-            opcaoCadastro = int(input('Digite a opção escolhida -> )
-
-            match (opcaoCadastro):
-                case 1:
-                    print('-='*25)
-                    print('CADASTRAR CONDOMÍNIO E GESTOR')
-                    print('-='*25)
-                    
-                                 
-            classe = str(input('Informe
-
-        case 3:
-            print('-='*2)
-            print('DEV')
-            print('-='*2)
-
+            #ENTREGAR ID
+        case 0:
+            print('Saindo...')
         case __:
             print('Opção inváida! Por favor, tente novamente.')
             main()
